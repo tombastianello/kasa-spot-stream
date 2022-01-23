71ab8df7116bff14ad5eaa677938d0149432d361
Tommaso Bastianello <tom.bastianello@gmail.com>
2022-01-23 21:24:18 +0000

    Latest changes to support thumbnail images for HAS.

--- app/index.js
+++ app/index.js
@@ -3,20 +3,44 @@ exports.__esModule = true;
 var https = require("https");
 var express = require("express");
 var ffmpeg_stream_1 = require("ffmpeg-stream");
+var fs_1 = require("fs");
+var child_process_1 = require("child_process");
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 var KASA_USERNAME = process.env.KASA_USERNAME;
 var KASA_PASSWORD = process.env.KASA_PASSWORD;
 var SPOT_IP = process.env.SPOT_IP;
-// curl -vv -k -u 'tom.bastianello@gmail.com:VmVyemFAMTk1Nw==' --ignore-content-length "https://192.168.100.20:19443/https/stream/mixed?video=h264&audio=g711&resolution=hd" --output - | ffmpeg -y -i - test.mp4
+var KASA_AUTH_HEADER = "Basic " + Buffer.from(KASA_USERNAME + ":" + Buffer.from(KASA_PASSWORD).toString("base64")).toString("base64");
+if (!(KASA_USERNAME && KASA_PASSWORD && SPOT_IP)) {
+    throw new Error("You must provide the following environment variables: KASA_USERNAME, KASA_PASSWORD, SPOT_IP");
+}
 var app = express();
+var streamRunning = false;
+var getScreenshot = function () {
+    if (!streamRunning) {
+        try {
+            (0, fs_1.rmSync)("thumb.jpg");
+        }
+        catch (err) {
+            console.log("Skipping thumbnail as stream is in progress.");
+        }
+        console.log("Capturing thumbnail...");
+        (0, child_process_1.exec)("ffmpeg -i http://localhost:8080/video -ss 00:00:01.500 -f image2 -vframes 1 thumb.jpg").on("close", function () {
+            console.log("Thumbnail captured.");
+        });
+    }
+    setTimeout(function () {
+        getScreenshot();
+    }, 60 * 1000);
+};
 app.get('/video', function (req, resp) {
+    streamRunning = true;
     var streamReq = https.request({
         host: SPOT_IP,
         method: "GET",
         path: "/https/stream/mixed?video=h264&audio=g711&resolution=hd",
         port: 19443,
         headers: {
-            "Authorization": "Basic " + Buffer.from(KASA_USERNAME + ":" + Buffer.from(KASA_PASSWORD).toString("base64")).toString("base64")
+            "Authorization": KASA_AUTH_HEADER
         }
     }, function (res) {
         var converter = new ffmpeg_stream_1.Converter();
@@ -40,15 +64,24 @@ app.get('/video', function (req, resp) {
                 clearTimeout(timeout);
             }
             timeout = setTimeout(function () {
-                console.log("Stream ended.");
+                console.log("Stream timeout.");
                 converter.kill();
                 streamReq.destroy();
             }, 1000);
         });
+        req.on("close", function () {
+            console.log("Client has closed the remote connection (close).");
+            console.log("Stream ended.");
+            converter.kill();
+            streamReq.destroy();
+            streamRunning = false;
+        });
     });
     streamReq.end();
 });
-app.get("/stream", function (req, res) {
-    res.send("\n    <html>\n        <body>\n            <video id=\"videoPlayer\" controls>\n                <source src=\"http://localhost:8080/video\">\n            </video>\n        </body>\n    </html>\n    ");
+app.get('/thumbnail', function (req, resp) {
+    resp.header("Content-Type", "image/jpeg").send((0, fs_1.readFileSync)("./thumb.jpg"));
 });
 app.listen(8080);
+console.log("Listening on port 8080");
+getScreenshot();
--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,8 @@
 import * as https from "https";
 import * as express from "express";
 import { Converter } from "ffmpeg-stream";
+import { readFileSync, rmSync } from "fs";
+import { exec } from "child_process";
 
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 
@@ -8,20 +10,40 @@ const KASA_USERNAME: string = process.env.KASA_USERNAME;
 const KASA_PASSWORD: string = process.env.KASA_PASSWORD;
 const SPOT_IP: string = process.env.SPOT_IP;
 
-if(!(KASA_USERNAME && KASA_PASSWORD && SPOT_IP)) {
+const KASA_AUTH_HEADER = `Basic ${Buffer.from(`${KASA_USERNAME}:${Buffer.from(KASA_PASSWORD).toString("base64")}`).toString("base64")}`;
+
+if (!(KASA_USERNAME && KASA_PASSWORD && SPOT_IP)) {
     throw new Error("You must provide the following environment variables: KASA_USERNAME, KASA_PASSWORD, SPOT_IP");
 }
 
 let app = express();
 
+let streamRunning = false;
+const getScreenshot = () => {
+    if(!streamRunning) {
+        try {
+            rmSync("thumb.jpg")
+        }catch (err) {
+            console.log("Skipping thumbnail as stream is in progress.");
+        }
+        console.log("Capturing thumbnail...");
+        exec("ffmpeg -i http://localhost:8080/video -ss 00:00:01.500 -f image2 -vframes 1 thumb.jpg").on("close", () => {
+            console.log("Thumbnail captured.");
+        });
+    }
+    setTimeout(() => {
+        getScreenshot();
+    }, 60 * 1000);
+}
 app.get('/video', function (req, resp) {
+    streamRunning = true;
     let streamReq = https.request({
         host: SPOT_IP,
         method: "GET",
         path: "/https/stream/mixed?video=h264&audio=g711&resolution=hd",
         port: 19443,
         headers: {
-            "Authorization": `Basic ${Buffer.from(`${KASA_USERNAME}:${Buffer.from(KASA_PASSWORD).toString("base64")}`).toString("base64")}`
+            "Authorization": KASA_AUTH_HEADER
         }
     }, (res) => {
         const converter = new Converter();
@@ -48,14 +70,26 @@ app.get('/video', function (req, resp) {
                 clearTimeout(timeout);
             }
             timeout = setTimeout(() => {
-                console.log("Stream ended.");
+                console.log("Stream timeout.");
                 converter.kill();
                 streamReq.destroy();
             }, 1000);
+        });
+        req.on("close", () => {
+            console.log("Client has closed the remote connection (close).");
+            console.log("Stream ended.");
+            converter.kill();
+            streamReq.destroy();
+            streamRunning = false;
         });
     });
     streamReq.end();
 });
 
-app.listen(8080)
+app.get('/thumbnail', function (req, resp) {
+    resp.header("Content-Type", "image/jpeg").send(readFileSync("./thumb.jpg"));
+});
+
+app.listen(8080);
 console.log("Listening on port 8080");
+getScreenshot();
